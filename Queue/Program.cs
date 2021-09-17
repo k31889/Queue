8e39d2671dab7b97b67f13acfd5d85c69037bc5d
7tsvn <x7tsvn@hotmail.com>
2021-09-17 17:27:10 +0100

    added files

---
+++ Queue/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Q s = new Q(5);
+            s.Add("Hello");
+            s.Print();
+        }
+    }
+}
---
+++ Queue/Q.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Queue
+{
+    class Q
+    {
+        private string[] Queue;
+        private bool IsFull;
+
+        private int front;
+        private int back;
+        public Q (int s)
+        {
+            Queue = new string[s];
+            front = 0;
+            back = 0;
+            IsFull = false;
+        }
+
+        public bool Add(string s)
+        {
+            bool added = true;
+            if(IsFull == false)
+            {
+                Queue[back] = s;
+                back = (back + 1) % Queue.Length;
+                if (front == back)
+                {
+                    added = false;
+                    IsFull = true;
+                }
+            }
+            return added;
+        }
+        public string Remove()
+        {
+            string value = "";
+            if(front == back && IsFull == false)
+            {
+                Console.WriteLine("Cannot remove, queue is empty");
+            }
+            else
+            {
+                value = Queue[front];
+                front = (front + 1) % Queue.Length;
+            }
+            return value;
+        }
+
+        public void Print()
+        {
+            int i = front;
+            do
+            {
+                Console.WriteLine(Queue[i]);
+                i = (i + 1) % Queue.Length;
+            }
+            while (i != back);
+        }
+    }
+}
